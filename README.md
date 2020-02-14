72e5d60f424049a35c896f29e2545e02be377179
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2020-02-14 19:22:26 +0300

    based code from drunk dev

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# dynamic-day
-DynamicDay is simple Tera Toolbox module which adds a dynamic environment to Tera Online based on the player’s local time.
+# Dynamic day
+
+Dynamic day is simple Tera Toolbox module which adds a dynamic environment to Tera Online based on the player’s local or server time.
+
+## Installation
+
+Manual installation:
+- Create a folder called `dynamic-day` in `<tera-toolbox>/mods`, download current repository and place it in created folder
+
+Automatic installation 2:
+- Open Tera Toolbox, open `Get More Mods` tab and install it as simple mod.
+
+## Usage
+
+By default player local time will be used. 
+
+You can change this behavior with command `/8 dn`
+
+## Settings
+
+-	"timeType": "local" or "server" - aero based on local time or server
+-	"updateTryDelay": 1800000 - aero change try delay (30mins by default, value in milliseconds)
+-	"delayAtSpawn": 3000 - fix for broken aero with slow pcs
+-	"transitionTime": 5 - switch abstract value (no clue what is dat, 1 is fast, 10 is slow :) )
+
+## Credits 
+
+- [HugeDong69](https://github.com/codeagon) original idea in past
---
+++ index.js
@@ -0,0 +1,95 @@
+
+const { DateTime } = require("luxon");
+
+const aeroData = require("./data/aero.json");
+const tz = require("./data/tz.json");
+
+class DynamicDayTime {
+	constructor(mod) {
+		const serverTZ = tz[mod.region];
+
+		let intervalRef = undefined;
+		let lastAero = undefined;
+		let lastDayPart = undefined;
+
+		const sendAero = (aero, blendTime) => {
+			mod.send("S_AERO", 1, {
+				"enabled": true,
+				"blendTime": blendTime,
+				"aeroSet": aero
+			});
+		};
+
+		const resetAero = () => {
+			mod.send("S_SPAWN_NPC", 11, {
+				gameId: 696969696969,
+				templateId: 8005000, 
+				huntingZoneId: 1023,
+				spawnScript: 105,
+				visible: true
+			})
+			mod.send("S_DESPAWN_NPC", 3, {
+				gameId: 696969696969,
+				type: 1
+			})
+		}
+
+		const applyAero = (aero, blendTime) => {
+			resetAero();
+			sendAero(aero, blendTime);
+		};
+
+		const getCurrentDayState = () => {
+			let hours = 0;
+			switch(mod.settings.timeType) {
+				case("local"): hours = DateTime.local().hour; break;
+				case("server"): hours = serverTZ ? DateTime.utc().setZone(serverTZ).hour : DateTime.local().hour; break;
+				default : hours = DateTime.local().hour; break;
+			}
+			if ((hours >= 23 && hours <= 24) || (hours <= 4)) return "night";
+			else if (hours >= 5 && hours <= 10) return "morning";
+			else if (hours >= 11 && hours <= 16) return "day";
+			else if (hours >= 17 && hours <= 23) return "evening";
+		}
+
+		const generateAero = () => {
+			let newDayPart = getCurrentDayState();
+
+			if (newDayPart === lastDayPart) return;
+
+			let arr = aeroData.enumAero[newDayPart];
+			let newAero = arr[Math.floor(Math.random() * arr.length)];
+
+			if (lastAero !== newAero) {
+				lastAero = newAero;
+				lastDayPart = newDayPart;
+
+				applyAero(newAero, mod.settings.transitionTime)
+			}
+		};
+
+		mod.hook("S_LOGIN", "event", () => { 
+			intervalRef = mod.setInterval(generateAero, mod.settings.updateTryDelay);
+		});
+
+		mod.hook("S_RETURN_TO_LOBBY", "event", () => {
+			mod.clearInterval(intervalRef);
+		});
+		
+		mod.hook("S_SPAWN_ME", "event", () => {
+			mod.setTimeout(()=> {
+				if(lastAero === undefined)
+					generateAero();
+				else
+					applyAero(lastAero, 1);
+			}, mod.settings.delayAtSpawn);
+		});
+
+		
+		mod.command.add("dn", {
+			"$none": () => { mod.settings.timeType = mod.settings.timeType === "local" ? "server" : "local"; }
+		})
+	}
+}
+
+module.exports = DynamicDayTime;
